43da802aa44a3705c12aaa2e047ec375a43e5eda
lotockib <brad.lotocki@maxar.com>
2023-06-22 17:37:47 -0600

    moved changed from manual copy verion to forked version

--- README.md
+++ README.md
@@ -1,105 +1,56 @@
-# Route Planning Project
+# Project
 
-This repo contains the starter code for the Route Planning project.
+This directory is a manual copy of [CppND-Route-Planning-Project](https://github.com/udacity/CppND-Route-Planning-Project) to allow for editing and project submission.
 
-<img src="map.png" width="600" height="450" />
+# Setup
 
-## Cloning
+1. Install dependencies per [Udacity repo instructions](https://github.com/udacity/CppND-Route-Planning-Project/blob/master/README.md#dependencies-for-running-locally), except for building io2d, follow these modified steps to avoid errors
+    ```
+    sudo apt update
+    sudo apt install build-essential
+    sudo apt install cmake
+    sudo apt install libcairo2-dev
+    sudo apt install libgraphicsmagick1-dev
+    sudo apt install libpng-dev
 
-When cloning this project, be sure to use the `--recurse-submodules` flag. Using HTTPS:
-```
-git clone https://github.com/udacity/CppND-Route-Planning-Project.git --recurse-submodules
-```
-or with SSH:
-```
-git clone git@github.com:udacity/CppND-Route-Planning-Project.git --recurse-submodules
-```
+    git submodule add git@github.com:cpp-io2d/P0267_RefImpl.git
+    cd P0267_RefImpl
+    git submodule update --init --recursive
+    code CMakeListst.txt -> comment out last two sections on TESTS and SAMPLES (why?)
+    mkdir build
+    cd build
+    cmake --config Release "-DCMAKE_BUILD_TYPE=Release" ..
+    make
+    sudo make install
+    ```
 
-## Dependencies for Running Locally
-* cmake >= 3.11.3
-  * All OSes: [click here for installation instructions](https://cmake.org/install/)
-* make >= 4.1 (Linux, Mac), 3.81 (Windows)
-  * Linux: make is installed by default on most Linux distros
-  * Mac: [install Xcode command line tools to get make](https://developer.apple.com/xcode/features/)
-  * Windows: [Click here for installation instructions](http://gnuwin32.sourceforge.net/packages/make.htm)
-* gcc/g++ >= 7.4.0
-  * Linux: gcc / g++ is installed by default on most Linux distros
-  * Mac: same instructions as make - [install Xcode command line tools](https://developer.apple.com/xcode/features/)
-  * Windows: recommend using [MinGW](http://www.mingw.org/)
-* IO2D
-  * Installation instructions for all operating systems can be found [here](https://github.com/cpp-io2d/P0267_RefImpl/blob/master/BUILDING.md)
-  * This library must be built in a place where CMake `find_package` will be able to find it
- 
+2. Clone this repo and initialize submodules.
 
-## Compiling and Running
+    ```
+    git clone https://github.com/lotockib/nanodegree.git --recurse-submodules
+    ```
+3. Build.
 
-### Compiling
-To compile the project, first, create a `build` directory and change to that directory:
-```
-mkdir build && cd build
-```
-From within the `build` directory, then run `cmake` and `make` as follows:
-```
-cmake ..
-make
-```
-### Running
-The executable will be placed in the `build` directory. From within `build`, you can run the project as follows:
-```
-./OSM_A_star_search
-```
-Or to specify a map file:
-```
-./OSM_A_star_search -f ../<your_osm_file.osm>
-```
+    ```
+    cd nanodegree/0_cpp_foundations/4_route_planner_project/CppND-Route-Planning-ProjectCopy/
+    mkdir build
+    cd build
+    cmake ..
+    make
+    ```
 
-## Testing
+# Test
 
-The testing executable is also placed in the `build` directory. From within `build`, you can run the unit tests as follows:
 ```
+cd build
 ./test
 ```
 
-## Troubleshooting
-* Some students have reported issues in cmake to find io2d packages, make sure you have downloaded [this](https://github.com/cpp-io2d/P0267_RefImpl/blob/master/BUILDING.md#xcode-and-libc).
-* For MAC Users cmake issues: Comment these lines from CMakeLists.txt under P0267_RefImpl
-    ```
-    if( NOT DEFINED IO2D_WITHOUT_SAMPLES )
-	     add_subdirectory(P0267_RefImpl/Samples)
-    endif()
-    ```
-    And then run "ALL_Build" and "install" in XCode.
-    
-    If any packages are missing try to install packages using 
-    ```
-    brew install pkg-config
-    ```
- * For Ubuntu Linux IO2D installation errors, follow the given steps:
-   ```
-	sudo apt update
-	sudo apt install build-essential
-	sudo apt install cmake
-	sudo apt install libcairo2-dev
-	sudo apt install libgraphicsmagick1-dev
-	sudo apt install libpng-dev
+# Run
 
-	git clone --recurse-submodules https://github.com/cpp-io2d/P0267_RefImpl
-	cd P0267_RefImpl
-	mkdir Debug
-	cd Debug
-	cmake --config Debug "-DCMAKE_BUILD_TYPE=Debug" ..
-	cmake --build .
-	sudo make install
-   ```
-     
- * If you are working on windows and unable to install IO2D:
-      * Enable WSL (Windows Subsystem for Linux) and use a distribution like [Ubuntu](https://ubuntu.com/wsl).(available from the windows store): 
-      * Install the required dependencies (compiler, cmake etc.) in the WSL(as mentioned above for ubuntu)
-      * Configure CLion to use the WSL [toolchain](https://www.jetbrains.com/help/clion/how-to-use-wsl-development-environment-in-product.html#wsl-tooclhain)
-      * Use the WSL toolchain to build the project
-      * If you are still facing errors, visit this [link](https://github.com/udacity/CppND-Route-Planning-Project/issues/9).
-     
+```
+cd build
+./OSM_A_star_search
+```
 
-* If you are facing errors with --config try to remove -- from the command.
-
-
+<img src="map.png" width="600" height="450" />
--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,84 +1,164 @@
+// Copyright 2023
+
 #include "route_planner.h"
 #include <algorithm>
 
-RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+RoutePlanner::RoutePlanner(
+    RouteModel &model,
+    float start_x,
+    float start_y,
+    float end_x,
+    float end_y) : m_Model(model) {
     // Convert inputs to percentage:
     start_x *= 0.01;
     start_y *= 0.01;
     end_x *= 0.01;
     end_y *= 0.01;
 
-    // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
-    // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-
+    // Find closest existing nodes to user-provided start and end coordinates
+    start_node = &model.FindClosestNode(start_x, start_y);
+    end_node = &model.FindClosestNode(end_x, end_y);
 }
 
 
-// TODO 3: Implement the CalculateHValue method.
-// Tips:
-// - You can use the distance to the end_node for the h value.
-// - Node objects have a distance method to determine the distance to another node.
-
+/**
+ * Return h-value of node based on distance to end_node.
+*/
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
+    // Find distance from current node to end node
+    return node->distance(*end_node);
 }
 
-
-// TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
-// Tips:
-// - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
-// - Use CalculateHValue below to implement the h-Value calculation.
-// - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
-
+/**
+ * Add neighbor nodes that haven't yet been visisted.
+*/
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+    // Find neighbors of current node and populate vector
+    current_node->FindNeighbors();
 
+    // For each neightbor node
+    for (auto& neighbor : current_node->neighbors) {
+        // Don't process if already visited
+        if (neighbor->visited != true) {
+            // Set parent to current
+            neighbor->parent = current_node;
+
+            // Find distance between current node and this neighbor
+            neighbor->g_value =
+                current_node->g_value + current_node->distance(*neighbor);
+
+            // Find h value
+            neighbor->h_value = RoutePlanner::CalculateHValue(neighbor);
+
+            // Mark visited`
+            neighbor->visited = true;
+
+            // Add to the open list
+            open_list.push_back(neighbor);
+        }
+    }
 }
 
 
-// TODO 5: Complete the NextNode method to sort the open list and return the next node.
-// Tips:
-// - Sort the open_list according to the sum of the h value and g value.
-// - Create a pointer to the node in the list with the lowest sum.
-// - Remove that node from the open_list.
-// - Return the pointer.
+/**
+ * Compare nodes using g + h value.
+*/
+bool Compare(RouteModel::Node * node0, RouteModel::Node * node1) {
+    // Return true if node0 is closer, false otherwise
+    return node0->g_value + node0->h_value > node1->g_value + node1->h_value;
+}
 
+
+/**
+ * Sort open_list and return node with lowest cost.
+*/
 RouteModel::Node *RoutePlanner::NextNode() {
+    // Initialize variables
+    RouteModel::Node * next_node = nullptr;
 
+    // Sort open_list Option A: use dedicated function
+    // sort(open_list.begin(), open_list.end(), Compare);
+
+    // Sort open_list option B: use lambda (better)
+    sort(
+        open_list.begin(),
+        open_list.end(),
+        []
+        (const RouteModel::Node* a, const RouteModel::Node* b)
+        {return a->g_value + a->h_value > b->g_value + b->h_value;
+        });
+
+    // Get lowest cost node for return
+    next_node = open_list.back();
+    open_list.pop_back();
+
+    return next_node;
 }
 
+/**
+ * Use provided node and parents to construct path, and calculate
+ * total distance.
+*/
+std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(
+    RouteModel::Node *current_node) {
 
-// TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
-// Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
-//   chain of parents of nodes until the starting node is found.
-// - For each node in the chain, add the distance from the node to its parent to the distance variable.
-// - The returned vector should be in the correct order: the start node should be the first element
-//   of the vector, the end node should be the last element.
-
-std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
-    // Create path_found vector
+    // Initialize variables
     distance = 0.0f;
-    std::vector<RouteModel::Node> path_found;
+    std::vector<RouteModel::Node> path_found = {*current_node};
+    RouteModel::Node * parent = nullptr;
 
-    // TODO: Implement your solution here.
+    // Step through each parent to construct start-to-end path vector
+    while (true) {
+        // Get parent
+        parent = current_node->parent;
 
-    distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
+        // If parent doesn't exist, we're at the starting node, exit loop
+        if (parent == nullptr)
+            break;
+
+        // Add parent to path option A
+        // path_found.insert(path_found.begin(), *parent);
+
+        // Add parent to path option B, more memory efficient per big O notation
+        path_found.push_back(*parent);
+
+        // Update cumulative distance
+        distance += current_node->distance(*parent);
+
+        // Move to parent node for next iteration
+        current_node = parent;
+    }
+
+    // Convert to meters, and return path in descending order
+    distance *= m_Model.MetricScale();
+    std::reverse(path_found.begin(), path_found.end());
     return path_found;
-
 }
 
-
-// TODO 7: Write the A* Search algorithm here.
-// Tips:
-// - Use the AddNeighbors method to add all of the neighbors of the current node to the open_list.
-// - Use the NextNode() method to sort the open_list and return the next node.
-// - When the search has reached the end_node, use the ConstructFinalPath method to return the final path that was found.
-// - Store the final path in the m_Model.path attribute before the method exits. This path will then be displayed on the map tile.
-
+/**
+ * Perform A* Search
+*/
 void RoutePlanner::AStarSearch() {
-    RouteModel::Node *current_node = nullptr;
+    // Initialize starting node
+    RouteModel::Node *current_node;
+    current_node = start_node;
+    current_node->h_value = CalculateHValue(current_node);
+    current_node->g_value = 0;
+    current_node->visited = true;
 
-    // TODO: Implement your solution here.
+    // Sort open_list and return next node
+    while (true) {
+        // Add neighbors of current_node to open_list
+        AddNeighbors(current_node);
 
+        // Sort open list and find next node
+        current_node = NextNode();
+
+        // Check if at end_node
+        if (current_node == end_node)
+            break;
+    }
+
+    // Construct finel path
+    m_Model.path = ConstructFinalPath(current_node);
 }
